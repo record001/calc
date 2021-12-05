bc3fcfa521c2076056a460a25c524941cf63259f
record001 <umarkhidirboev@gmail.com>
2021-12-05 16:33:42 +0500

    intialCh

---
+++ javaS.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+// document.getElementById("zero").addEventListener("click",function getZero(){
+//     let x =Number(document.getElementById("zero").innerHTML); 
+//     // console.log(x);
+//     document.getElementById("text").value += x;
+//     // alert("sasadfa");
+// } );
+
+
+
+function salom(param){
+    // param.preventDefault();
+    // alert(param);
+    document.getElementById("text").value += param;
+}
+
+
+// document.getElementById("one").addEventListener("click", function gugu(){
+//     salom(1);
+// });
+// document.getElementById("two").addEventListener("click", function gugu(){
+//     salom(2);
+// });
+// document.getElementById("three").addEventListener("click", function gugu(){
+//     salom(3);
+// });
+// document.getElementById("four").addEventListener("click", function gugu(){
+//     salom(4);
+// });
+// document.getElementById("five").addEventListener("click", function gugu(){
+//     salom(5);
+// });
